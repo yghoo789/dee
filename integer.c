f4a78c36be27382492012e9046ebda78b8b10209
yghoo789 <yghoo789@naver.com>
2016-09-26 17:44:39 +0900

    Add files via upload

---
+++ integer.c
@@ -0,0 +1,34 @@
+﻿ /* 
+ 솔루션 / 프로젝트 / 소스파일: Ch03 / Prj05 / integer.c 
+ 정수형 자료형 변수의 선언과 활용 
+ V 1.0 2016. 
+ */ 
+ 
+ 
+ #include <stdio.h> 
+ 
+ 
+ int main(void) 
+ { 
+ 	short sVar = 32000; 
+ 	int	  iVar = -2140000000; 
+ 
+ 
+ 	unsigned short int	usVar = 65000; 
+ 	unsigned int		uiVar = 4280000000; 
+ 
+ 
+ 	printf("저장값: %d %d\n", sVar, iVar); 
+	printf("저장값: %u %u\n", usVar, uiVar); 
+ 
+ 
+ 	long long dist1 = 2720000000000; 
+ 	__int64 dist2 = 4500000000000; 
+ 
+ 
+ 	printf("지구와 천왕성 간의 거리(km): %lld\n", dist1); 
+ 	printf("태양와 해왕성 간의 거리(km): %lld\n", dist2); 
+ 
+ 
+ 	return 0; 
+ } 
